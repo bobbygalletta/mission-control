3c7fc6b879f06de6bb1f4c80ebf2856a151fb607
Dean <dean@openclaw>
2026-03-30 21:56:36 -0400

    Auto-reload: version extracted from SW URL, only reloads after a rebuild

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,7 @@
 const VER = '3';
-const CACHE = 'mc-v3';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
-  e.waitUntil(Promise.all([
-    caches.keys().then(k => Promise.all(k.map(caches.delete))),
-    self.clients.claim(),
-  ]));
+  e.waitUntil(Promise.all([caches.keys().then(k => Promise.all(k.map(caches.delete))), self.clients.claim()]));
 });
 self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
